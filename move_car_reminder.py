21e8c33f7ad579ea3c5d40c5fbdf7a93d28547af
jdonohue44 <jared.donohue@gmail.com>
2017-07-26 21:39:34 -0400

    complete script

--- move_car_reminder.py
+++ move_car_reminder.py
@@ -1,2 +1,29 @@
 # This script will run the day before the first Friday of every month, reminding user to move their car for street cleaning.
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
+source = 'espressmorningnews@gmail.com'
+dest = 'jared.donohue@gmail.com'
+
+message = MIMEMultipart()
+message['From'] = source
+message['To'] = dest
+message['Subject'] = 'STREET CLEANING TOMORROW'
+
+body = MIMEText("Street cleaning occurs on Brookline street the first Friday of every month.", 'plain')
+message.attach(body)
+
+try:
+    smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp_server.starttls()
+    smtp_server.login(source, '5638JabroniStreet**')
+except Exception:
+    print("ERROR connecting to email server")
+
+# send email
+try:
+   smtp_server.sendmail(source, dest, message.as_string())
+   smtp_server.quit()
+except smtplib.SMTPException:
+   print("Error: unable to send email")
